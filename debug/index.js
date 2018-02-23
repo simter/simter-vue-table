9f92aa1f8d61901f5c49be26522e65f77257ff4f
RJ.Hwang <rongjihuang@gmail.com>
2018-02-23 18:20:31 +0800

    Polishing debug

--- debug/index.js
+++ debug/index.js
@@ -1,21 +1,47 @@
 import table from '../src/table.vue'
 
 window.onload = function () {
-  Vue.component('st-table', table)
-
   new Vue({
-    el: "#demo1",
+    el: "#sample1",
     data: {
       columns: [
-        { id: "id1", label: "Column1", width: "100px" },
-        { id: "id2", label: "Column2", width: "150px" },
-        { id: "id3", label: "Column3", width: "200px" }
+        { id: "key1", label: "Column1", width: "100px" },
+        { id: "key2", label: "Column2", width: "120px" },
+        { id: "key3", label: "Column3", width: "150px" }
       ],
       rows: [
-        { id1: "v11", id2: "v21", id3: "v31" },
-        { id1: "v12", id2: "v22", id3: "v32" },
-        { id1: "v13", id2: "v23", id3: "v33" }
+        { key1: "v1-1", key2: "v2-1", key3: "v3-1" },
+        { key1: "v1-2", key2: "v2-2", key3: "v3-2" },
+        { key1: "v1-3", key2: "v2-3", key3: "v3-3" }
       ]
+    },
+    components: {
+      "st-table": table
+    }
+  })
+
+  new Vue({
+    el: "#sample2",
+    data: {
+      columns: [
+        { id: "key1", label: "Column1", width: "100px" },
+        {
+          label: "Column2",
+          children: [
+            { id: "key21", label: "Column21", width: "120px" },
+            { id: "key22", label: "Column22", width: "130px" }
+          ]
+        },
+        { id: "key3", label: "Column3", width: "150px" }
+      ],
+      rows: [
+        { key1: "v1-1", key21: "v2-1-1", key22: "v2-2-1", key3: "v3-1" },
+        { key1: "v1-2", key21: "v2-1-2", key22: "v2-2-2", key3: "v3-2" },
+        { key1: "v1-3", key21: "v2-1-3", key22: "v2-2-3", key3: "v3-3" }
+      ]
+    },
+    components: {
+      "st-table": table
     }
   })
 }
