a64b540520b9f3c4960abf14466ca794386aec04
RJ.Hwang <rongjihuang@gmail.com>
2018-02-23 10:14:44 +0800

    Simple implementation

---
+++ docs/index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  Vue.component("st-table", window["simter-vue-table"]);
+
+  new Vue({
+    el: "#demo1",
+    data: {
+      columns: [
+        { id: "id1", label: "Column1", width: "100px" },
+        { id: "id2", label: "Column2", width: "100px" },
+        { id: "id3", label: "Column3", width: "100px" }
+      ],
+      rows: [
+        { id1: "v11", id2: "v21", id3: "v31" },
+        { id1: "v12", id2: "v22", id3: "v32" },
+        { id1: "v13", id2: "v23", id3: "v33" }
+      ]
+    }
+  });
+};
---
+++ src/table.vue
@@ -0,0 +1,35 @@
+<template>
+<table class="st-table">
+  <colgroup is="st-colgroup" :columns="columns"></colgroup>
+  <thead is="st-thead" :columns="columns"></thead>
+  <tbody>
+    <tr v-for="(row, index) in rows" :key="row.id || index">
+      <td v-for="column in columns" :key="column.id">{{row[column.id]}}</td>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+<script>
+import colgroup from "simter-vue-colgroup";
+import thead from "simter-vue-thead";
+
+export default {
+  replace: true,
+  props: {
+    columns: { type: Array, required: true },
+    rows: { type: Array, required: false, default: [] }
+  },
+  components: {
+    "st-colgroup": colgroup,
+    "st-thead": thead
+  }
+};
+</script>
+
+<style>
+.st-table {
+  border-collapse: collapse;
+  table-layout: fixed;
+}
+</style>
