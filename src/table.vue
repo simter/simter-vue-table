c2c7eb9d3abb1f2b0b42e5446de31766338a4dcc
RJ.Hwang <rongjihuang@gmail.com>
2018-03-01 15:45:36 +0800

    Support group column

--- src/table.vue
+++ src/table.vue
@@ -1,10 +1,10 @@
 <template>
-<table class="st-table">
+<table>
   <colgroup is="st-colgroup" :columns="columns"></colgroup>
   <thead is="st-thead" :columns="columns"></thead>
   <tbody>
     <tr v-for="(row, index) in rows" :key="row.id || index">
-      <td v-for="column in columns" :key="column.id">{{row[column.id]}}</td>
+      <td v-for="column in columnsLeaf" :key="column.id">{{row[column.id]}}</td>
     </tr>
   </tbody>
 </table>
@@ -14,22 +14,55 @@
 import colgroup from "simter-vue-colgroup";
 import thead from "simter-vue-thead";
 
-export default {
+const defaultClasses = { table: "st-table" };
+const component = {
   replace: true,
   props: {
     columns: { type: Array, required: true },
-    rows: { type: Array, required: false, default: [] }
+    rows: {
+      type: Array,
+      required: false,
+      default() {
+        return [];
+      }
+    }
+  },
+  computed: {
+    /**
+     * The result of function call flatten(this.columns).
+     * It gather all columns leaf to return.
+     */
+    columnsLeaf() {
+      return flatten(this.columns);
+    }
   },
   components: {
     "st-colgroup": colgroup,
     "st-thead": thead
   }
 };
-</script>
 
-<style>
-.st-table {
-  border-collapse: collapse;
-  table-layout: fixed;
+/**
+ * Flatten all leaf object to simple object array.
+ *
+ * Example :
+ * 1. [{id: 1}, {id: 2}]
+ *    flatten to
+ *    [{id: 1}, {id: 2}]
+ * 2. [{id: 1}, {children: [{id: 2}, {id: 3}]}]
+ *    flatten to
+ *    [{id: 1}, {id: 2}, {id: 3}]
+ * 3. [{id: 1}, {children: [{id: 2}, {children: [{id: 3}, {id: 4}]}]}]
+ *    flatten to
+ *    [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
+ */
+function flatten(columns) {
+  return columns.reduce(
+    (a, b) => a.concat(b.children ? flatten(b.children) : b),
+    []
+  );
 }
-</style>
+
+export { flatten };
+export default component;
+</script>
