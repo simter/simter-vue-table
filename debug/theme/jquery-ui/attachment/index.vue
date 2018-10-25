2fb73c67b5a2a85f2c16cf0e3c79a09c5014bdfa
RJ.HWang <rongjihuang@gmail.com>
2018-10-25 17:36:01 +0800

    Add group demo on attachment

--- debug/theme/jquery-ui/attachment/index.vue
+++ debug/theme/jquery-ui/attachment/index.vue
@@ -4,6 +4,7 @@
     <h2 class="ui-priority-primary" style="margin: 0 0 .5em 0">
       事故附件
       <span class="ui-priority-secondary" style="font-size: 50%"><a href="https://jqueryui.com">jQuery UI Integration</a></span>
+      <label><input type="checkbox" v-model="ui.group"> 分组显示</label>
       <div class="ui-priority-secondary" style="float: right; font-size: 60%">Theme :
         <select class="ui-widget-content" style="padding: 0.2em; display: inline-block; width: auto"
           v-model="ui.theme" @change="changeTheme">
@@ -13,26 +14,23 @@
     </h2>
   </div>
   <div class="template">
-    <st-table :columns="columns" :rows="rows" :classes="classes"
-              @cell-change="cellChange">
+    <st-table :columns="columns" :rows="rows"
+      :classes="classes" :group="group"
+      @cell-change="cellChange">
     </st-table>
-  </div>
-  <div class="options" style="border: none">
-    <label>Options:</label>
-    <ul style="list-style: none; padding: 0; margin: 0">
-      <li><label><input type="checkbox" v-model="ui.borderedTable"> Bordered table</label></li>
-    </ul>
   </div>
 </form>
 </template>
 
 <script>
 import table from "../../../../src/table.vue";
+const groupConfig = {prop: "group", names: ["流程类", "诉讼类"]};
 export default {
   data() {
     return {
       ui: {
         borderedTable: true,
+        group: true,
 
         theme: "dark-hive",
         themes: {
@@ -40,54 +38,60 @@ export default {
           "dark-hive": "https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/dark-hive/jquery-ui.min.css"
         }
       },
+
       columns: [
         { id: "sn", label: "序号", width: "3em", 
           cell: {
-            type: 'st-cell-row-picker', 
-            showContent: true, 
+            component: 'st-cell-row-picker', 
+            showContent: false, 
+            showRowNumber: true, 
             pickedProp: 'picked', 
             classes: { label: 'label', input: 'ui-widget-content', span: 'tt' }
           }
         },
         { id: "name", label: "名称", width: "16em", 
           cell: {
-            type: 'st-cell-text-editor', 
+            component: 'st-cell-text-editor', 
             classes: { input: 'ui-widget-content' }
           }
         },
         { id: "ext", label: "类型", width: "3em", cell: { class: "ext" } },
         { label: "大小", children: [
-          { id: "size", label: "大小", width: "4em", cell: { type: 'st-cell-number-editor', classes: {input: 'ui-widget-content'} } },
+          { id: "size", label: "大小", width: "4em", cell: { component: 'st-cell-number-editor', classes: {input: 'ui-widget-content'} } },
           { id: "unit", label: "单位", width: "3em" }
         ]},
         { id: "updateTime", label: "更新时间", width: "13em", 
           cell: { 
-            type: 'st-cell-html', 
+            component: 'st-cell-html', 
             render: function(value, row) { return `${value} <i><u>${row.updater}</u></i>`}
           }
         }
       ],
       rows: [
-        {sn: 1, name: "现场照片1", ext: 'png', size: "1", unit: "KB", updater: "黄小明", updateTime: "2018-01-01 12:30", group: "现场照片"},
-        {sn: 2, name: "现场照片2", ext: 'jpg', size: "10", unit: "KB", updater: "黄小张", updateTime: "2018-01-01 12:35", group: "现场照片"},
-        {sn: 1, name: "维修类照片1", ext: 'png', size: "1", unit: "MB", updater: "黄小明", updateTime: "2018-01-01 12:30", group: "维修类"},
-        {sn: 2, name: "维修类照片2", ext: 'jpg', size: "1.5", unit: "MB", updater: "黄小张", updateTime: "2018-01-01 12:40", group: "维修类"},
-        {sn: 3, name: "维修记录", ext: 'docx', size: "2", unit: "MB", updater: "黄小张", updateTime: "2018-01-01 12:50", group: "维修类"}
+        {sn: 11, group: "现场照片", name: "现场照片1", ext: 'png', size: "1", unit: "KB", updater: "黄小明", updateTime: "2018-01-01 12:30"},
+        {sn: 12, group: "现场照片", name: "现场照片2", ext: 'jpg', size: "10", unit: "KB", updater: "黄小张", updateTime: "2018-01-01 12:35"},
+        {sn: 21, group: "维修类", name: "维修类照片1", ext: 'png', size: "1", unit: "MB", updater: "黄小明", updateTime: "2018-01-01 12:30"},
+        {sn: 22, group: "维修类", name: "维修类照片2", ext: 'jpg', size: "1.5", unit: "MB", updater: "黄小张", updateTime: "2018-01-01 12:40"},
+        {sn: 23, group: "维修类", name: "维修记录", ext: 'docx', size: "2", unit: "MB", updater: "黄小张", updateTime: "2018-01-01 12:50"}
       ],
-      group: "group",
+      group: groupConfig,
       classes: {
         thead: {
           tr: "st-header ui-widget-content",
           th: "st-cell"
         },
-        row: "st-row ui-widget-content"
+        row: "st-row ui-widget-content",
+        groupRow: "st-group-row ui-widget-content"
       }
     };
   },
   components: {
     "st-table": table
   },
-  computed: {
+  watch: {
+    'ui.group': function(value){
+        this.$set(this, 'group', value ? groupConfig : '');
+    }
   },
   mounted(){
     // get all themes
