d4f2001830d661a4e13a36cbebc9f00a576ac557
RJ <rongjihuang@gmail.com>
2018-11-01 09:28:56 +0800

    Demo  for group component

--- debug/theme/jquery-ui/attachment/index.vue
+++ debug/theme/jquery-ui/attachment/index.vue
@@ -30,7 +30,15 @@
 
 <script>
 import table from "../../../../src/table.vue";
-const groupConfig = {prop: "group", names: ["流程类", "诉讼类"]};
+const groupConfig = {
+  prop: "group",
+  component: "st-cell-text-editor",
+  names: ["流程类", "诉讼类"],
+  cell: {
+    classes: { input: "ui-widget-content" },
+    styles: { input: "width: 22.1em" }
+  }
+};
 export default {
   data() {
     return {
@@ -40,44 +48,113 @@ export default {
 
         theme: "dark-hive",
         themes: {
-          "base": "https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/base/jquery-ui.min.css",
-          "dark-hive": "https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/dark-hive/jquery-ui.min.css"
+          base:
+            "https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/base/jquery-ui.min.css",
+          "dark-hive":
+            "https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/dark-hive/jquery-ui.min.css"
         }
       },
 
       columns: [
-        { id: "sn", label: "序号", width: "3em", 
+        {
+          id: "sn",
+          label: "序号",
+          width: "3em",
           cell: {
-            component: 'st-cell-row-picker', 
-            showContent: false, 
-            showRowNumber: true, 
-            classes: { label: 'label', input: 'ui-widget-content', span: 'tt' }
+            component: "st-cell-row-picker",
+            showContent: false,
+            showRowNumber: true,
+            classes: { label: "label", input: "ui-widget-content", span: "tt" }
           }
         },
-        { id: "name", label: "名称", width: "16em", 
+        {
+          id: "name",
+          label: "名称",
+          width: "16em",
           cell: {
-            component: 'st-cell-text-editor', 
-            classes: { input: 'ui-widget-content' }
+            component: "st-cell-text-editor",
+            classes: { input: "ui-widget-content" }
           }
         },
         { id: "ext", label: "类型", width: "3em", cell: { class: "ext" } },
-        { label: "大小", children: [
-          { id: "size", label: "大小", width: "4em", cell: { component: 'st-cell-number-editor', classes: {input: 'ui-widget-content'} } },
-          { id: "unit", label: "单位", width: "3em" }
-        ]},
-        { id: "updateTime", label: "更新时间", width: "13em", 
-          cell: { 
-            component: 'st-cell-html', 
-            render: function(value, row) { return `${value} <i><u>${row.updater}</u></i>`}
+        {
+          label: "大小",
+          children: [
+            {
+              id: "size",
+              label: "大小",
+              width: "4em",
+              cell: {
+                component: "st-cell-number-editor",
+                classes: { input: "ui-widget-content" }
+              }
+            },
+            { id: "unit", label: "单位", width: "3em" }
+          ]
+        },
+        {
+          id: "updateTime",
+          label: "更新时间",
+          width: "13em",
+          cell: {
+            component: "st-cell-html",
+            render: function(value, row) {
+              return `${value} <i><u>${row.updater}</u></i>`;
+            }
           }
         }
       ],
       rows: [
-        {sn: 11, group: "现场照片", name: "现场照片1", ext: 'png', size: "1", unit: "KB", updater: "黄小明", updateTime: "2018-01-01 12:30"},
-        {sn: 12, group: "现场照片", name: "现场照片2", ext: 'jpg', size: "10", unit: "KB", updater: "黄小张", updateTime: "2018-01-01 12:35"},
-        {sn: 21, group: "维修类", name: "维修类照片1", ext: 'png', size: "1", unit: "MB", updater: "黄小明", updateTime: "2018-01-01 12:30"},
-        {sn: 22, group: "维修类", name: "维修类照片2", ext: 'jpg', size: "1.5", unit: "MB", updater: "黄小张", updateTime: "2018-01-01 12:40"},
-        {sn: 23, group: "维修类", name: "维修记录", ext: 'docx', size: "2", unit: "MB", updater: "黄小张", updateTime: "2018-01-01 12:50"}
+        {
+          sn: 11,
+          group: "现场照片",
+          name: "现场照片1",
+          ext: "png",
+          size: "1",
+          unit: "KB",
+          updater: "黄小明",
+          updateTime: "2018-01-01 12:30"
+        },
+        {
+          sn: 12,
+          group: "现场照片",
+          name: "现场照片2",
+          ext: "jpg",
+          size: "10",
+          unit: "KB",
+          updater: "黄小张",
+          updateTime: "2018-01-01 12:35"
+        },
+        {
+          sn: 21,
+          group: "维修类",
+          name: "维修类照片1",
+          ext: "png",
+          size: "1",
+          unit: "MB",
+          updater: "黄小明",
+          updateTime: "2018-01-01 12:30"
+        },
+        {
+          sn: 22,
+          group: "维修类",
+          name: "维修类照片2",
+          ext: "jpg",
+          size: "1.5",
+          unit: "MB",
+          updater: "黄小张",
+          updateTime: "2018-01-01 12:40"
+        },
+        {
+          sn: 23,
+          group: "维修类",
+          name: "维修记录",
+          ext: "docx",
+          size: "2",
+          unit: "MB",
+          updater: "黄小张",
+          updateTime: "2018-01-01 12:50"
+        }
       ],
       group: groupConfig,
       classes: {
@@ -95,40 +172,64 @@ export default {
     "st-table": table
   },
   watch: {
-    'ui.group': function(value){
-        this.$set(this, 'group', value ? groupConfig : '');
+    "ui.group": function(value) {
+      this.$set(this, "group", value ? groupConfig : "");
     }
   },
-  mounted(){
+  mounted() {
     // get all themes
-    let url = "https://data.jsdelivr.com/v1/package/npm/jquery-ui-built-themes@1.12.1";
+    let url =
+      "https://data.jsdelivr.com/v1/package/npm/jquery-ui-built-themes@1.12.1";
     fetch(url)
-    .then(response => response.json())
-    .then(result => {
-      console.log("find %s themes from %s", result.files.length, url);
-      result.files.filter(theme => theme.type === "directory")
-      .forEach(theme => {
-        if(!this.ui.themes.hasOwnProperty(theme.name)) this.$set(this.ui.themes, theme.name, 
-          `https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/${theme.name}/jquery-ui.min.css`);
+      .then(response => response.json())
+      .then(result => {
+        console.log("find %s themes from %s", result.files.length, url);
+        result.files
+          .filter(theme => theme.type === "directory")
+          .forEach(theme => {
+            if (!this.ui.themes.hasOwnProperty(theme.name))
+              this.$set(
+                this.ui.themes,
+                theme.name,
+                `https://cdn.jsdelivr.net/npm/jquery-ui-built-themes@1.12.1/${
+                  theme.name
+                }/jquery-ui.min.css`
+              );
+          });
       });
-    })
   },
   methods: {
-    changeTheme(){
-      document.getElementById('theme').href = this.ui.themes[this.ui.theme];
+    changeTheme() {
+      document.getElementById("theme").href = this.ui.themes[this.ui.theme];
     },
     cellChange($event) {
-      console.log("cell change RC[%d, %d]: newValue=%s, oldValue=%s, $event=%o", $event.rowIndex, $event.columnIndex, $event.newValue, $event.oldValue, $event);
+      console.log(
+        "cell change RC[%d, %d]: newValue=%s, oldValue=%s, $event=%o",
+        $event.rowIndex,
+        $event.columnIndex,
+        $event.newValue,
+        $event.oldValue,
+        $event
+      );
     },
     cellClick($event) {
-      console.log("cell click RC[%d, %d]: target=%s", $event.rowIndex, $event.columnIndex, $event.target);
+      console.log(
+        "cell click RC[%d, %d]: target=%s",
+        $event.rowIndex,
+        $event.columnIndex,
+        $event.target
+      );
     },
     deleteSelection($event) {
       this.$refs.myTable.deleteSelection();
     },
     selectionChange(selection, oldValue) {
-      this.selectionNames = selection.map(r => r.name).join(", ")
-      console.log("selectionChange: new=%s, old=%s", this.selectionNames, oldValue.map(r => r.name).join(", "));
+      this.selectionNames = selection.map(r => r.name).join(", ");
+      console.log(
+        "selectionChange: new=%s, old=%s",
+        this.selectionNames,
+        oldValue.map(r => r.name).join(", ")
+      );
     }
   }
 };
