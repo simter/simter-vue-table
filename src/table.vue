4e32195775922f79b61c5149a8cc7eb8670d3f13
RJ <rongjihuang@gmail.com>
2018-11-02 16:02:47 +0800

    Seperate flatten function to a standalone file

--- src/table.vue
+++ src/table.vue
@@ -59,6 +59,7 @@
 <script>
 import colgroup from "simter-vue-colgroup";
 import thead from "simter-vue-thead";
+import flatten from "./utils/flatten";
 import groupRows from "./utils/group";
 
 // inner cell components
@@ -166,25 +167,5 @@ const component = {
   }
 };
 
-/**
- * Flatten all leaf object to simple object array.
- *
- * Example :
- * 1. [{id: 1}, {id: 2}]
- *    flatten to
- *    [{id: 1}, {id: 2}]
- * 2. [{id: 1}, {children: [{id: 2}, {id: 3}]}]
- *    flatten to
- *    [{id: 1}, {id: 2}, {id: 3}]
- * 3. [{id: 1}, {children: [{id: 2}, {children: [{id: 3}, {id: 4}]}]}]
- *    flatten to
- *    [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
- */
-function flatten(columns) {
-  return columns.reduce(
-    (a, b) => a.concat(b.children ? flatten(b.children) : b),
-    []
-  );
-}
 export default component;
 </script>
---
+++ src/utils/flatten.js
@@ -0,0 +1,22 @@
+/**
+ * Flatten all leaf object to simple object array.
+ *
+ * Example :
+ * 1. [{id: 1}, {id: 2}]
+ *    flatten to
+ *    [{id: 1}, {id: 2}]
+ * 2. [{id: 1}, {children: [{id: 2}, {id: 3}]}]
+ *    flatten to
+ *    [{id: 1}, {id: 2}, {id: 3}]
+ * 3. [{id: 1}, {children: [{id: 2}, {children: [{id: 3}, {id: 4}]}]}]
+ *    flatten to
+ *    [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
+ */
+function flatten(columns) {
+  return columns.reduce(
+    (a, b) => a.concat(b.children ? flatten(b.children) : b),
+    []
+  );
+}
+
+export default flatten;
