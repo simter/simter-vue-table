ab92baf8f9f8cd23ab7e01bee4d5068eda4c392c
RJ <rongjihuang@gmail.com>
2018-11-16 15:02:00 +0800

    Fixed 'rollup -c' warning

--- rollup.config.js
+++ rollup.config.js
@@ -11,15 +11,10 @@ let banner = `/*!
 // see http://vuejs.github.io/rollup-plugin-vue/#/en/2.3/?id=configuration
 export default {
   input: 'src/table.vue',
+  external: ['vue'],
   output: [
-    {
-      file: pkg.main, format: 'umd', name: pkg.name, banner: banner,
-      globals: {
-        "simter-vue-colgroup": "simter-vue-colgroup",
-        "simter-vue-thead": "simter-vue-thead"
-      }
-    },
-    { file: pkg.module, format: 'es', banner: banner }
+    { file: pkg.main, format: 'umd', name: pkg.name, banner: banner, exports: "named"},
+    { file: pkg.module, format: 'es', banner: banner, exports: "named" }
   ],
   plugins: [
     resolve(),
